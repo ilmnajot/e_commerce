00adbb3d7e9fea0e0fe1307edcfedab5c349d7a7
Elbekjon <96689605+ilmnajot@users.noreply.github.com>
2024-09-02 22:02:56 +0500

    e_commerce | 02.09.2024 |

---
+++ src/test/java/uz/ilmnajot/ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.ilmnajot.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
